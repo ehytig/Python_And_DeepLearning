122ff25da73a424022026f27026fd4157747f084
Chenyu <lx10@mail.ustc.edu.cn>
2021-04-11 17:29:19 +0800

    Finish the construction for classes in utils.py

--- homework1/utils.py
+++ homework1/utils.py
@@ -1,27 +1,49 @@
 import requests
 from homework1.HtmlPraser import get_url
+import shutil
+from homework1.RisOperation import generate_name_from_ris
+import os 
 
 class web_file:
-    def __init__(self, url=None, name="temporary", postfix = None):
+    def __init__(self, url=None, name="temporary", postfix = None, directory = None):
         self.url = url
         self.postfix = postfix
+        if not directory:
+            self.directory = directory
+        else:
+            self.directory = os.getcwd()
+
 
     def download(self, link = None , path = None ):
         if not link:
             link = self.url
         if not path:
-            path = self.name + self.postfix
+            path = self.path()
         # download file from link, and name it with the name variable
         r = requests.get(link) 
         with open(path ,'wb') as f:
             f.write(r.content)
 
-    def url_get():
+    def url_get(self):
         raise Exception("You don't define the url_get()")
 
+    def rename(self, newname):
+        oldpath = self.path()
+        self.name = newname
+        newpath = self.path()
+        shutil.move(oldpath, newpath)
+
+    def path(self):
+        if not self.directory :
+            path = self.directory + self.name + self.postfix
+        else:
+            path = self.name + self.postfix
+        return path
+
+        
 class pdf(web_file):
-    def __init__(self):
-       web_file.__init__(self, postfix = ".pdf")
+    def __init__(self, directory = None):
+       web_file.__init__(self, postfix = ".pdf", directory= directory)
 
     def url_get(self,web_url):
 
@@ -43,8 +65,8 @@ class pdf(web_file):
 
 
 class ris(web_file):
-    def __init__(self):
-       web_file.__init__(self, postfix = ".ris")
+    def __init__(self, directory = None):
+       web_file.__init__(self, postfix = ".ris", directory= directory)
     
     
     def url_get(self,web_url):
@@ -65,17 +87,26 @@ class ris(web_file):
         self.url = get_url(rislink_filter, web_url)
 
 class paper():
-    def __init__(self,web_url):
+    def __init__(self, web_url, pdf_directory = None, ris_directory = None):
         self.url = web_url
-        self.pdf = pdf()
-        self.ris = ris()
+        self.pdf_dir = pdf_directory
+        self.ris_dir = ris_directory
+        self.pdf = pdf(directory= self.pdf_dir)
+        self.ris = ris(directory= self.ris_dir)
 
     def download(self):
-        pdf.url_get(web_url)
-        pdf.download()
+        self.pdf.url_get(web_url)
+        self.pdf.download()
 
-        ris.url_get(web_url)
-        ris.download()
+        self.ris.url_get(web_url)
+        self.ris.download()
+
+        # generate name from .ris file and rename both pdf and ris files
+        name = generate_name_from_ris(ris.path())
+        self.pdf.rename(name)
+        self.ris.rename(name)
+        
+
 
 
         
