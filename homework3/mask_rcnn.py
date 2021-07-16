76d1d540a0a37c1a2a245c449f43f2f8c49ceb1d
ehytig <lx10@mail.ustc.edu.cn>
2021-07-16 22:44:08 +0800

    Add img write function in code

--- homework3/mask_rcnn.py
+++ homework3/mask_rcnn.py
@@ -86,6 +86,35 @@ trainer.resume_or_load(resume=False)
 trainer.train()
 
 
+# inference
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+from detectron2.utils.visualizer import ColorMode
+dataset_dicts = get_balloon_dicts("/data/awslcy/balloon/val")
+os.mkdir("/output/infer_img")
+for d in dataset_dicts:    
+    fpath,fname = os.path.split(d["file_name"])
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=balloon_metadata, 
+                   scale=0.5, 
+                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imwrite("/output/infer_img/"+fname, out.get_image()[:, :, ::-1])
+
+# evaluation
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+evaluator = COCOEvaluator("balloon_val", ("bbox", "segm"), False, output_dir="/output/")
+val_loader = build_detection_test_loader(cfg, "balloon_val")
+print(inference_on_dataset(trainer.model, val_loader, evaluator))
+    
+
+
 
 
 
